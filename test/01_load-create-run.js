018219ec818f901739e5964448fd7af9c8f03950
Hilmar Lapp <hlapp@drycafe.net>
2017-02-26 00:59:13 +0000

    Fixes startup so that runtime API can be used directly

--- test/01_load-create-run.js
+++ test/01_load-create-run.js
@@ -8,8 +8,7 @@ var http = require('http'),
     path = require('path'),
     EventEmitter = require('events'),
     express = require('express'),
-    request = require('request'),
-    delay = require('delay');
+    embeddedStart = require('node-red-embedded-start');
 var test = require('tape');
 
 const restPrefix = require('crypto').randomBytes(8).toString('hex');
@@ -28,7 +27,7 @@ const settings = {
 };
 const LISTEN_PORT = process.env.PORT || 8080;
 const LISTEN_IF = "127.0.0.1";
-const DELAY_AFTER_START = 2500;
+const FLOWS_TIMEOUT = 5000;
 
 var before = test;
 var server;
@@ -40,32 +39,6 @@ const SENSOR_NODE = 'wirelesstag';
 const ALL_NODE = 'wirelesstag-all';
 const CONFIG_NODE = 'wirelesstag-config';
 const NODE_TYPES = [CONFIG_NODE, SENSOR_NODE, ALL_NODE];
-
-function invokeAdminAPI(methodName, reqBody, httpMethod) {
-    if (! httpMethod) httpMethod = 'GET';
-    if (httpMethod === 'GET' && reqBody) {
-        methodName += '/' + reqBody;
-        reqBody = undefined;
-    }
-    let uriPath = `${settings.httpAdminRoot}/${methodName}`;
-    let apiCall = new Promise((resolve, reject) => {
-        request({
-            method: httpMethod,
-            uri: `http://${LISTEN_IF}:${LISTEN_PORT}${uriPath}`,
-            json: true,
-            jar: true,
-            body: reqBody
-        }, function (error, response, body) {
-            if (error || response.statusCode > 204) {
-                console.error(body);
-                if (! error) error = new Error("status " + response.statusCode);
-                return reject(error);
-            }
-            resolve(body);
-        });
-    });
-    return apiCall;
-}
 
 function failAndEnd(t) {
     return (err) => {
@@ -107,7 +80,7 @@ test.onFinish(function() {
 });
 
 before('can create and initialize Node-RED runtime', function(t) {
-    // t.plan(4);
+    t.plan(5);
 
     RED = require('node-red');
     t.ok(RED, 'instantiates Node-RED runtime');
@@ -124,10 +97,10 @@ before('can create and initialize Node-RED runtime', function(t) {
     app.use(settings.httpNodeRoot, RED.httpNode);
     server.listen(LISTEN_PORT, LISTEN_IF);
 
-    RED.start().then(() => {
+    RED.start().then((result) => {
         t.pass('starts Node-RED runtime');
-        t.comment('delaying briefly so flows can start');
-    }).then(delay(DELAY_AFTER_START)).then(t.end).catch(failAndEnd(t));
+        return embeddedStart(RED, FLOWS_TIMEOUT, result);
+    }).then(() => t.pass("flows started")).catch(failAndEnd(t));
 });
 
 test('our nodes are registered', function(t) {
@@ -145,6 +118,7 @@ test('our nodes are registered', function(t) {
 });
 
 test('our nodes can be created and added to a flow', function(t) {
+    t.plan(9);
 
     NODE_TYPES.forEach((nt) => {
         let conf = { id: RED.util.generateId(), type: nt };
@@ -165,30 +139,33 @@ test('our nodes can be created and added to a flow', function(t) {
         nodes: [nodes[SENSOR_NODE], nodes[ALL_NODE]],
         configs: [nodes[CONFIG_NODE]]
     };
-    invokeAdminAPI('flow', flow, 'POST').then((result) => {
-        t.ok(result, 'successfully created flow with our nodes');
-        t.ok(result.id, 'received ID of newly created flow');
-        testFlow = result.id;
-        return invokeAdminAPI('flow', testFlow);
+    RED.nodes.addFlow(flow).then((id) => {
+        t.ok(id, 'successfully created flow with our nodes');
+        testFlow = id;
+        return RED.nodes.getFlow(testFlow);
     }).then((result) => {
         t.equal(result.id, testFlow, 'can retrieve the created flow');
         t.equal(result.nodes.length, 2, 'new flow has 2 nodes');
         t.equal(result.configs.length, 1, 'new flow has 1 config node');
+
         t.equal(result.configs[0].type, 'wirelesstag-config',
                 'config node is of our type');
         nodes[result.configs[0].type] = result.configs[0];
+
         for (let i = 0; i < result.nodes.length; i++) {
+            nodes[result.nodes[i].type] = result.nodes[i];
             t.equal(result.nodes[i].cloud,
                     result.configs[0].id,
                     `node ${i} references correct cloud config`);
             t.equal(result.nodes[i].type.indexOf('wirelesstag'), 0,
                     `node ${i} is of one of the types we define`);
-            nodes[result.nodes[i].type] = result.nodes[i];
         }
     }).then(t.end).catch(failAndEnd(t));
 });
 
 test('once added to a flow nodes can be retrieved from runtime', function(t) {
+    t.plan(12);
+
     for (let nt in nodes) {
         if (nodes[nt].id) {
             let n = RED.nodes.getNode(nodes[nt].id);
